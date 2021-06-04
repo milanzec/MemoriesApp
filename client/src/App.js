8afd8cec866c14873ebdbe0b39d2c740654aa6dd
Your Name <you@example.com>
2021-06-04 22:34:57 +0200

    components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 
 const App = () => {
   return (
     <div>
-      <h1>hellou</h1>
+      <Posts />
+      <Form />
     </div>
   );
 };
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Post from './Post/Post';
+
+const Posts = () => {
+  return (
+    <div>
+      <h1>Posts</h1>
+      <h2>
+        <Post />
+      </h2>
+    </div>
+  );
+};
+
+export default Posts;
