f5482cd04a3d28a711a1e462559c3c520e88a135
Your Name <you@example.com>
2021-06-07 18:31:06 +0200

    App and styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import { Grid, Container, Grow, Typography, AppBar } from '@material-ui/core';
+import memories from './images/memories.png';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 
+import useStyles from './styles';
+
 const App = () => {
   return (
-    <div>
-      <Posts />
-      <Form />
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height="100" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="strech"
+            spacing={4}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
